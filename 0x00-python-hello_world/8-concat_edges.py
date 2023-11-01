31e2bb63e4e69c90596cd3bc8c2b4e4924f4d746
Soumia El Mousstari <elmousstarisoumia@gmail.com>
2023-11-01 05:40:34 -0700

    8. Create a new sentence

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[39:67], end = " ")
-print("with Python")
+str = str[39:67] + " with Python"
+print(str)
