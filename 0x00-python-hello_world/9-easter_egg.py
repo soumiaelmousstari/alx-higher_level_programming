fdff6aa638037f0eaff9db867ccc53142acbc9c1
Soumia El Mousstari <elmousstarisoumia@gmail.com>
2023-11-01 07:05:01 -0700

    9. Easter Egg

---
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+s = "better"
+s1 = f"is {s} than"
+str = f"The Zen of Python, by Tim Peters\n\
+Beautiful {s1} ugly.\n\
+Explicit {s1} implicit.\n\
+Simple {s1} complex.\n\
+Complex {s1} complicated.\n\
+Flat {s1} nested.\n\
+Sparse {s1} dense.\n\
+Readability counts.\n\
+Special cases aren't special enough to break the rules.\n\
+Although practicality beats purity.\n\
+Errors should never pass silently.\n\
+Unless explicitly silenced.\n\
+In the face of ambiguity, refuse the temptation to guess.\n\
+There should be one-- and preferably only one --obvious way to do it.\n\
+Although that way may not be obvious at first unless you're Dutch.\n\
+Now {s1} never.\n\
+Although never is often {s} than *right* now.\n\
+If the implementation is hard to explain, it's a bad idea.\n\
+If the implementation is easy to explain, it may be a good idea.\n\
+Namespaces are one honking great idea -- let's do more of those!"
+print(str)
