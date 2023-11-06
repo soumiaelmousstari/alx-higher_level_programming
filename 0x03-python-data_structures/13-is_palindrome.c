6e987f8885f1aaf45d4b7daedb37fb525140888d
Soumia El Mousstari <elmousstarisoumia@gmail.com>
2023-11-06 13:49:08 -0800

    13. Linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -10,20 +10,20 @@ int is_palindrome(listint_t **head)
 {
 	if (head == NULL || *head == NULL)
 		return (1);
-	return (aux_palind(head, *head));
+	return (palind(head, *head));
 }
 
 /**
- * aux_palind - funct to know if is palindrome
+ * palind - funct to know if is palindrome
  * @head: head list
  * @end: end list
- * Return: 1
+ * Return: 1, 0
 */
-int aux_palind(listint_t **head, listint_t *end)
+int palind(listint_t **head, listint_t *end)
 {
 	if (end == NULL)
 		return (1);
-	if (aux_palind(head, end->next) && (*head)->n == end->n)
+	if (palind(head, end->next) && (*head)->n == end->n)
 	{
 		*head = (*head)->next;
 		return (1);
