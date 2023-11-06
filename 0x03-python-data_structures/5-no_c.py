92dca96ed31f90d66cf361a05df00332be2ba594
Soumia El Mousstari <elmousstarisoumia@gmail.com>
2023-11-06 11:38:13 -0800

    5. Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@ def no_c(my_string):
     ret = ""
     for i in range (len(my_string)):
         if (my_string[i] != 'c' and my_string[i] !='c'):
-            ret += my+string[i]
+            ret += my_string[i]
     return (ret)
