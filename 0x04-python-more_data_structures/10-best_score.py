0fa874e37c97f8fa3ae3d553d5eee747873c8634
Soumia El Mousstari <elmousstarisoumia@gmail.com>
2023-11-08 12:45:03 -0800

    all is here

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,6 +3,7 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return (None)
     maxval = 0
+    maxkey = None
     for k, v in a_dictionary.items():
         if v > maxval:
             maxval = v
