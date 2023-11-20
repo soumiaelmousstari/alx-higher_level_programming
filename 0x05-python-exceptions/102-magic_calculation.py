35347e19200a9594ca0cb9011574e318a283a1ea
Soumia El Mousstari <elmousstarisoumia@gmail.com>
2023-11-20 07:18:24 -0800

    all here

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    resulta = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            resulta += a ** b / i
+        except Exception:
+            resulta = b + a
+            break
+        return (resulta)
