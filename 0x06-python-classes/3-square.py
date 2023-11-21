4fc4df497fdb130cf73dc403632348077659df79
Soumia El Mousstari <elmousstarisoumia@gmail.com>
2023-11-21 13:04:03 -0800

    all here

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -12,12 +12,13 @@ class Square:
             TypeError: If size is not an integer.
             ValueError: If size is less than 0.
         """
-        if not isinstance (size, int):
+        if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
         self.__size = size
-    def area (self):
+
+    def area(self):
         """Area of this square.
         Returns:
             The size squared.
