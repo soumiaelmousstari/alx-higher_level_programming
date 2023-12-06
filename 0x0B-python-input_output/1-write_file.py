c22c176494fe7d03d46e212b7d74630b9ec3b8ee
Soumia El Mousstari <elmousstarisoumia@gmail.com>
2023-12-06 14:43:23 -0800

    all hetre

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""write_file module."""
+
+
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file (UTF8) and
+    returns the number of characters written.
+    """
+    with open(filename, 'w') as f:
+        return f.write(text)
---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""append_write module."""
+
+
+def append_write(filename="", text=""):
+    """
+    Appends a string at the end of a text file (UTF8)
+    and returns the number of characters added.
+    """
+    with open(filename, 'a') as f:
+        return f.write(text)
