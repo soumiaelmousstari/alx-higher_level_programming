61fc8943c10a95dabb8ebd10f58d8f17a9327908
Soumia El Mousstari <elmousstarisoumia@gmail.com>
2024-02-12 13:23:42 -0800

    7. I love C

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const nbr = Math.floor(Number(process.argv[2]));
+if (isNaN(nbr)){
+	console.log('Missing number of occurrences');
+}
+else{
+	for(let i = 0; i < nbr; i++){
+		console.log('C is fun');
+	}
+}
