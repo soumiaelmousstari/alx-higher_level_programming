216c238a42485d484c768d33af6152db205148de
Soumia El Mousstari <elmousstarisoumia@gmail.com>
2024-02-12 14:02:20 -0800

    8. Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 const nbr = Math.floor(Number(process.argv[2]));
 if (isNaN(nbr)) {
-	console.log('Missing size');
+  console.log('Missing size');
 } else {
-	for (let i = 0; i < nbr; i++) {
-		let str = '';
-		for (let j = 0; j < nbr; j++) {
-			str += 'X';
-		}
-		console.log(str);
-	}
+  for (let i = 0; i < nbr; i++) {
+    let str = '';
+    for (let j = 0; j < nbr; j++) {
+      str += 'X';
+    }
+    console.log(str);
+  }
 }
