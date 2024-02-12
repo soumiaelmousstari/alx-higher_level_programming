1a00fd4492af90e83891dc9ad773e8f4d904a336
Soumia El Mousstari <elmousstarisoumia@gmail.com>
2024-02-12 13:51:57 -0800

    2. Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const nbr_arg = process.argv.length - 2;
-console.log(nbr_arg === 0 ? 'No argument' : nbr_arg === 1 ? 'Argument found' : 'Arguments found');
+const nbrarg = process.argv.length - 2;
+console.log(nbrarg === 0 ? 'No argument' : nbrarg === 1 ? 'Argument found' : 'Arguments found');
