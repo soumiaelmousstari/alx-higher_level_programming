177ab1c070bae0d6970bdcd3843bb54f74696f27
Soumia El Mousstari <elmousstarisoumia@gmail.com>
2024-02-13 12:34:41 -0800

    2. Rectangle #2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
+    if (isNaN(w) || isNaN(h) || w <= 0 || h <= 0) {
       return {};
     }
     this.width = w;
