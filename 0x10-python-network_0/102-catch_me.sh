7c982cd4b8bf66911035a29168d48f9f77c6b852
Soumia El Mousstari <elmousstarisoumia@gmail.com>
2024-02-29 03:13:01 -0800

    9. Catch me if you can!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #9. Catch me if you can!
-curl -sL 0.0.0.0:5000/catch_me -X PUT -d "You got me!" -H "Origin: HolbertonSchool"
+curl -sL 0.0.0.0:5000/catch_me -X PUT -d "user_id=98" -H "Origin: HolbertonSchool"
