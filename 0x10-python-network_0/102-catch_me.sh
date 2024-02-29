e134961464dedff2386f3d61060ecb1cfd030c40
Soumia El Mousstari <elmousstarisoumia@gmail.com>
2024-02-29 03:07:20 -0800

    9. Catch me if you can!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#a Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response
+#9. Catch me if you can!
 curl -sL 0.0.0.0:5000/catch_me -X PUT -d "You got me!" -H "Origin: HolbertonSchool"
