caa592db581b6ddc870b5b721d6ffef47f0386c5
Soumia El Mousstari <elmousstarisoumia@gmail.com>
2024-03-02 05:43:52 -0800

    1. Response header value #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 # a Python script that takes in a URL and an email
 # sends a POST request to the passed URL with the email as
-# a parameter, and displays the body of the response (decoded in utf-8
-import sys
-import urllib.parse
-import urllib.request
+# a parameter, and displays the body of the response (decoded in utf-8)
 
 if __name__ == "__main__":
+    import sys
+    import urllib.request
+    import urllib.parse
     url = sys.argv[1]
     value = {"email": sys.argv[2]}
     data = urllib.parse.urlencode(value).encode("ascii")
